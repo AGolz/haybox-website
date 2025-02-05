60c0d1bf8d410324e2a1ad8c354e767a38e33589
Alyona Golz <golz.alyona@gmail.com>
2025-02-05 14:00:20 +0400

    Update tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -1,24 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector(".tab-link").click();
+});
+
 function openTab(evt, tabName) {
   let i, tabcontent, tablinks;
   
-  // Скрываем все вкладки
   tabcontent = document.getElementsByClassName("tab-content");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Убираем активный класс у всех кнопок вкладок
   tablinks = document.getElementsByClassName("tab-link");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].classList.remove("active");
   }
 
-  // Показываем нужную вкладку и делаем кнопку активной
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.classList.add("active");
-}
-
-// Запускаем скрипт при загрузке страницы
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector(".tab-link").click();
-});

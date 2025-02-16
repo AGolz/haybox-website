a69641397577644b7abb8870eb8e8d53977adad2
Alyona Golz <golz.alyona@gmail.com>
2025-02-16 04:18:53 +0400

    Update translate.js

--- js/translate.js
+++ js/translate.js
@@ -1,10 +1,14 @@
 async function translateText(text, targetLang) {
+    console.log("Отправка запроса на перевод:", { text, targetLang });
+
     const response = await fetch("/.netlify/functions/translate", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ text, targetLang })
+        headers: { "Content-Type": "application/json" }, // добавляем заголовок
+        body: JSON.stringify({ text, targetLang }) // JSON.stringify - важно!
     });
 
     const data = await response.json();
+    console.log("Ответ от Netlify translate:", data);
+
     return data.translations ? data.translations[0].text : text;
 }

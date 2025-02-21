1bd771fb4c63e44a45bf15b3fb37338b26c56d61
Alyona Golz <golz.alyona@gmail.com>
2025-02-21 11:45:20 +0400

    Update floating-buttons.js

--- js/floating-buttons.js
+++ js/floating-buttons.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const chatButton = document.querySelector(".floating-button.chat");
-    const chatMenu = document.querySelector(".chat-menu");
+document.addEventListener("DOMContentLoaded", function () {
+	const chatButton = document.querySelector(".floating-button.chat");
+	const chatMenu = document.querySelector(".chat-menu");
 
-    chatButton.addEventListener("click", function(event) {
-        event.stopPropagation();
-        chatMenu.classList.toggle("active");
-    });
-  
-    document.addEventListener("click", function(event) {
-        if (!event.target.closest(".floating-button.chat")) {
-            chatMenu.classList.remove("active");
-        }
-    });
+	chatButton.addEventListener("click", function (event) {
+		event.stopPropagation();
+		chatMenu.classList.toggle("active");
+	});
+
+	document.addEventListener("click", function (event) {
+		if (!event.target.closest(".floating-button.chat")) {
+			chatMenu.classList.remove("active");
+		}
+	});
 });
